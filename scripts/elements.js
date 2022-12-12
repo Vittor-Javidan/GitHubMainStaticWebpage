f09983c952a9fad6958a11cc706aa10ed0620ecc
Vittor-Javidan <vittor.javidan.t@gmail.com>
2022-12-12 20:01:23 -0300

    window taskbar mvp finished

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,6 +1,11 @@
+//Sidebar related
 const sidebar = document.querySelector('.sidebar');
 const toggle = document.querySelector('.sidebar .toggle');
+const navItems = document.querySelectorAll('nav .nav-item')
 
+//Page related
 const pages = document.querySelectorAll('.page')
 const dragAreas = document.querySelectorAll('.draggable')
-const navItems = document.querySelectorAll('nav .nav-item')
+const redDots = document.querySelectorAll('.red-dot')
+const yellowDots = document.querySelectorAll('.yellow-dot')
+const greenDots = document.querySelectorAll('.green-dot')
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -6,7 +6,49 @@ navItems.forEach((navItem, index) => {
 
 	navItem.addEventListener('click', () => {
 
+		if(pages[index].classList.contains('close-page'))
+			pages[index].classList.remove('close-page')
+
 		navItem.classList.toggle('active')
 		pages[index].classList.toggle('hide-page')
 	})
+});
+
+redDots.forEach((redDot, index) => {
+
+	redDot.addEventListener('click', () => {
+
+		navItems[index].classList.toggle('active')
+		pages[index].classList.toggle('hide-page')
+		pages[index].classList.toggle('close-page')
+	})
+});
+
+yellowDots.forEach((yellowDot, index) => {
+
+	yellowDot.addEventListener('click', () => {
+
+		navItems[index].classList.toggle('active')
+		pages[index].classList.toggle('hide-page')
+	})
+});
+
+greenDots.forEach((greenDot, index) => {
+
+	greenDot.addEventListener('click', () => {
+
+		
+	})
+
+	greenDot.addEventListener('click', () => {
+
+		
+		pages.forEach(element => {
+			if(pages[index] !== element)
+            	element.classList.remove('fullscreen');
+        })
+
+		pages[index].classList.toggle('fullscreen')
+        
+    });
 });
